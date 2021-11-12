dffcead62f70f3ff98f6117d38fc19864cb3f8a8
braymix <michelepanarotto00@gmail.com>
2021-11-12 20:14:44 +0100

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root', // eslint-disable-line @angular-eslint/component-selector
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {}
